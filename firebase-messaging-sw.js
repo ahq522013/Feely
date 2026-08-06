2403b21f13e1db619780e53a00493b45672b71fe
ahq522013 <ahq522013@gmail.com>
2026-08-06 19:13:46 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,28 +1,25 @@
 importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
 
-// استبدل القيم بالقيم الخاصة بمشروعك
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyCZLJ3VX05H05lTFWt3obbqVK71_jgaffs",
   authDomain: "feely-813ca.firebaseapp.com",
   databaseURL: "https://feely-813ca-default-rtdb.firebaseio.com",
   projectId: "feely-813ca",
   storageBucket: "feely-813ca.firebasestorage.app",
   messagingSenderId: "945704178477",
-  appId: "1:945704178477:web:7ec595932e9e56699539a4",
-  measurementId: "G-N8JF3818SX"
-};
+  appId: "1:945704178477:web:7ec595932e9e56699539a4"
+});
 
-firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// التعامل مع الإشعارات القادمة والتطبيق مغلق
+// استقبال الإشعار عندما يكون التطبيق في الخلفية أو مغلقاً
 messaging.onBackgroundMessage((payload) => {
-    const notificationTitle = payload.notification.title || "نبضة حب جديدة! 💌";
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: 'https://cdn-icons-png.flaticon.com/512/2589/2589175.png'
-    };
+  const notificationTitle = payload.notification.title || 'نبضة حب جديدة 💖';
+  const notificationOptions = {
+    body: payload.notification.body || 'وصلتك نبضة جديدة في Feely!',
+    icon: 'https://cdn-icons-png.flaticon.com/512/2589/2589175.png'
+  };
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
